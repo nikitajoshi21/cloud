01d18e7a78ee328e138a3d2e052ba11499b9b5db
nikitajoshi21 <65816649+nikitajoshi21@users.noreply.github.com>
2020-06-14 12:14:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@ Welcome to Nikita Joshi Page
 <pre>
 <img src="https://d2d913y14vjptj.cloudfront.net/image" hieght= "200px" width= "200px">
 <?php
-print "cloud";
+print "cloud \n";
 print 'ifconfig';
 ?>
 </pre>
